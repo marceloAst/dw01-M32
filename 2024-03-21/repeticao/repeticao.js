2dc9330ec7f68fb2ab9dce47240de100695bdc71
marceloAst <marcelo.2007@alunos.utfpr.edu.br>
2024-09-19 09:32:52 -0300

    repeticao

---
+++ 2024-03-21/repeticao/repeticao.js
@@ -0,0 +1,13 @@
+let condicao1 = 10 > 2
+let condicao2 = true
+let d = false
+
+console.log("1")
+console.log("2")
+
+while(condicao1 && condicao2 || (d)){
+    console.log("A")
+}
+
+console.log("3")
+console.log("4")
---
+++ 2024-03-21/repeticao/repeticao2.js
@@ -0,0 +1,10 @@
+function mostrarTabuada (n){
+    let num  = 1
+    let a = 10
+    while(num < 10){
+        console.log(n * a)
+        num += 1
+    }
+}
+mostrarTabuada(5)
+mostrarTabuada(3)
